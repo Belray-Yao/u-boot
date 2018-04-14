2076830588355dbf33e561bb735068847d413a31
zyro <811405258@qq.com>
2018-04-14 18:50:29 +0800

    just led is on

--- arch/arm/include/asm/mach-types.h
+++ arch/arm/include/asm/mach-types.h
@@ -1107,6 +1107,7 @@ extern unsigned int __machine_arch_type;
 #define MACH_TYPE_COLIBRI_T30          4493
 #define MACH_TYPE_APALIS_T30           4513
 #define MACH_TYPE_OMAPL138_LCDK        2495
+#define MACH_TYPE_ITOP4412			   2496
 
 #ifdef CONFIG_ARCH_EBSA110
 # ifdef machine_arch_type
